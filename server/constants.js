b0f942f30947b1ceafb17edf699c9916c1b74230
adam-adair <aar2222@gmail.com>
2020-10-18 21:52:09 -0400

    added data validations and orderItem model

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,22 @@
 const mediums = ['Graphic Art','Sculpture', 'Installation', 'Performance Art']
 const orderStatuses = ['Created', 'Processing', 'Cancelled', 'Completed']
 
+const minRating = 0
+
+const maxRating = 5
+
+const minReviewLength = 2
+
+const maxReviewLength = 1500
+
+const defaultImagePath = '/public/img/default.jpg'
+
 module.exports = {
   mediums,
-  orderStatuses
+  orderStatuses,
+  minRating,
+  maxRating,
+  minReviewLength,
+  maxReviewLength,
+  defaultImagePath
 }
---
+++ server/db/models/OrderItem.js
@@ -0,0 +1,14 @@
+const { DOUBLE, UUID, UUIDV4, INTEGER } = require("sequelize")
+const db = require('../db')
+
+const OrderItem = db.define('orderItem', {
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
+  },
+  orderedPrice: DOUBLE,
+  orderedQuantity: INTEGER
+})
+
+module.exports = OrderItem
