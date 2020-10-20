ed61c0bcfccc25124dc604eb1db6154bde1aed07
Zaina Rodney <zainarodney@gmail.com>
2020-10-19 20:46:42 -0400

    setup files and exports

--- server/index.js
+++ server/index.js
@@ -4,7 +4,14 @@ const path = require('path');
 const app = express();
 
 // INSERT APP.USE STUFF HERE
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, './public')))
+
+
+app.use('/api', require('./api'))
 // INSERT ERROR HANDLING HERE
+
+
 
 module.exports = app;
