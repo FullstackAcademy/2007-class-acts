ae5ffa40277010a8aa072f76ea0d48393ce6d230
adam-adair <aar2222@gmail.com>
2020-10-21 11:26:18 -0400

    login form update; lil bit o' stylin'

--- client/components/LoginScreen.js
+++ client/components/LoginScreen.js
@@ -9,10 +9,16 @@ class LoginScreen extends Component {
       password: ''
     }
     this.handleLogin = this.handleLogin.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   handleLogin(ev) {
     ev.preventDefault()
+    console.log(this.state)
+  }
+
+  handleChange(ev) {
+    this.setState({...this.state, [ev.target.name]: ev.target.value})
   }
 
   render() {
@@ -21,8 +27,8 @@ class LoginScreen extends Component {
         <form id="login-form" onSubmit={this.handleLogin}>
           <h2>Log in</h2>
           <hr />
-          <input name="username" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
+          <input name="username" placeholder="Username" onChange={this.handleChange}/>
+          <input name="password" type="password" placeholder="Password" onChange={this.handleChange}/>
           <button type="submit" id="login-button">Log in </button>
         </form>
       </div>
