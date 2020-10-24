f3df28716f2d7357d186298eaa27a02915e77c6b
adam-adair <aar2222@gmail.com>
2020-10-24 17:22:35 -0400

    partial cart backend

---
+++ server/api/cart.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const { Cart, CartItem } = require('../db')
+
+router.post('/item/', async (req, res, next) => {
+  try {
+    //if they're not logged in, don't do anything to the db, just say OK
+    if(!req.user) res.sendStatus(200)
+    //but if they are,
+    else {
+      const cart = await Cart.findOrCreate({
+        where: { userId: req.user.id }
+      })
+      const { artworkId, quantity } = req.body
+      await CartItem.create({
+        artworkId,
+        quantity,
+        cartId: cart.id
+      })
+      res.sendStatus(201)
+    }
+  }
+  catch(err) {
+    console.log(err)
+    next(err);
+  }
+})
+
+module.exports = router
