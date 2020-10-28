2b1d0de6669da07e5b6d5ff6cd46af4923e06125
Jamil Kabir <jamilkabir85@gmail.com>
2020-10-28 05:21:27 -0400

    Added admin-users-management component

--- client/redux/actionConstants.js
+++ client/redux/actionConstants.js
@@ -1,6 +1,7 @@
 export const GET_ARTWORKS = 'GET_ARTWORKS';
 export const GET_ARTISTS = 'GET_ARTISTS';
 export const GET_GENRES = 'GET_GENRES';
+export const GET_USERS = "GET_USERS"
 
 
 export const SET_USER = 'SET_USER'
---
+++ client/redux/users.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { GET_USERS } from './actionConstants';
+
+// ACTION CREATORS
+export const _getUsers = users => {
+  return {
+    type: GET_USERS,
+    users
+  }
+};
+
+// THUNK CREATORS
+export const getUsers = () => {
+  return async (dispatch) => {
+    const users = await axios.get('/api/users');
+    dispatch(_getUsers(users.data));
+  }
+};
+
+export default function usersReducer(state = [], action) {
+  switch (action.type) {
+    case GET_USERS:
+      return action.users;
+    default:
+      return state;
+  }
+}
